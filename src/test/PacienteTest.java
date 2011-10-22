d33509bf05061358fc5c746d569d419d26b5cfc1
Hugo Chagas <hugo.guit@gmail.com>
2011-10-22 15:21:22 -0200

    Algumas pequenas correcoes

--- src/test/PacienteTest.java
+++ src/test/PacienteTest.java
@@ -20,85 +20,68 @@ public class PacienteTest {
 		PacienteDAO dao = new PacienteDAOImpl();
 		Paciente result;
 		String[] listCPF = {"12707668648", "88278873429", "62978731605", "50475833767", "72743056975"};
-		
+
 		for(int i=0; i<5; i++) {
 			Paciente p = new Paciente("p"+i, listCPF[i]);
-			
-			//Salvar
-			
+
+			//Salvar			
 			//Teste para verificar o tipo do objeto que sera persistido
 			assertTrue(p instanceof Paciente);
-					
+			
 			//Teste para verificar se os campos obrigatorios estao preenchidos
-			assertTrue(p.getNomePac() != null && p.getCpfPac() != null);
-						
+			assertTrue( p != null && p.getNomePac() != null && p.getCpfPac() != null);
+
 			//Teste para validar o CPF do Paciente a ser persistido
 			assertTrue(p.getCpfPac().length() == 11);
 			assertTrue(GenericValidator.validaCpf(p.getCpfPac()));
-						
+
 			//Teste para verficar se um CPF ja existe no banco antes de persistir o paciente
 			assertTrue(new PacienteDAOImpl().pesquisaPorCpf(p.getCpfPac()) == null);			
-			
-			result = dao.salvar(p);			
-			System.out.println("Paciente Id: "+result.getCodPac());						
-		}
-		
+
+			result = dao.salvar(p);					
+		}		
 		/*==================================================================================*/
-		//Pesquisar
-		
+		//Pesquisar		
 		//Obter a lista de paciente cadastrados
 		List<Paciente> lstPac = dao.getLista();
 		assertEquals(lstPac.size(), 5);
-		
+
 		//Testar a pesquisa por Nome do Paciente
 		List<Paciente> listaNomes = dao.pesquisaPorNome("p3");
 		assertEquals(listaNomes.get(0).getNomePac(), "p3");
-		
+
 		//Pesquisar Paciente usando um codigo invalido
 		assertTrue(dao.pesquisaPorCodigo(50L) == null);
-				
+
 		//Testar se o paciente existe
 		assertTrue(dao.pesquisaPorCodigo(1L) != null);
-		Paciente pac = dao.pesquisaPorCodigo(1L);
-		
-		
+		Paciente pac = dao.pesquisaPorCodigo(1L);		
 		/*==================================================================================*/
-		//Alterar
-		
+		//Alterar		
 		//Alterando os dados do paciente recuperado no teste anterior
 		pac.setNomePac("Maria");
 		pac.setCelPac("1234567890");
-		
+
 		//Teste para validar novamente o CPF na alteração
 		assertTrue(pac.getCpfPac().length() == 11);
 		assertTrue(GenericValidator.validaCpf(pac.getCpfPac()));
-		
+
 		result = dao.salvar(pac);
-		
+
 		//Teste de alteracao com um CPF invalido
 		result.setCpfPac("1234567890");
-		assertTrue(GenericValidator.validaCpf(pac.getCpfPac()) == false);
-		
-		
-		result = dao.salvar(result);
-		
-		System.out.println("Nome alterado: "+result.getNomePac());
-		
-		
+		assertTrue(GenericValidator.validaCpf(pac.getCpfPac()) == false);		
+		result = dao.salvar(result);		
 		/*==================================================================================*/
 		//Excluir
+		//Exclusao de paciente
+		assertTrue(new PacienteDAOImpl().excluir(result));
+		
 		//Tentativa de excluir paciente invalido
 		Paciente exc = new Paciente();
 		exc.setCodPac(25L); // codigo invalido
 		exc.setNomePac("Excluir");
 		exc.setCpfPac("53558366239");
-		
-		assertTrue(new PacienteDAOImpl().excluir(exc) == false);
-		
-		//Exclusao de paciente
-		assertTrue(new PacienteDAOImpl().excluir(result));
-		
-		
+		assertTrue(new PacienteDAOImpl().excluir(exc) == false);		
 	}
-	
 }
